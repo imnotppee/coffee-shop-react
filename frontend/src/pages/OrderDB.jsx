74e29b35b021723c046956f01515008d7635c45d
snowfake99 <67030043@kmitl.ac.th>
2025-03-26 19:04:30 +0700

    Dashborad

---
+++ frontend/src/pages/OrderDB.jsx
@@ -0,0 +1,6 @@
+<div className="grid grid-cols-8 grid-rows-10 gap-5">
+    <div className="col-span-2 row-span-8">1</div>
+    <div className="col-span-6 col-start-3">2</div>
+    <div className="col-span-4 row-span-6 col-start-3 row-start-2">4</div>
+    <div className="col-span-2 row-span-6 col-start-7 row-start-2">13</div>
+</div>
---
+++ frontend/src/pages/StockDB.jsx
@@ -0,0 +1,9 @@
+<div className="grid grid-cols-8 grid-rows-8 gap-5">
+    <div className="col-span-2 row-span-8">1</div>
+    <div className="col-span-6 col-start-3">2</div>
+    <div className="col-span-2 row-span-5 col-start-3 row-start-2">22</div>
+    <div className="col-span-2 row-span-3 col-start-5 row-start-2">23</div>
+    <div className="col-span-2 row-span-3 col-start-7 row-start-2">24</div>
+    <div className="col-span-2 row-span-3 col-start-5 row-start-5">25</div>
+    <div className="col-span-2 row-span-3 col-start-7 row-start-5">26</div>
+</div>
